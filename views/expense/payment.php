2fe63cd16d01aec0622788502a577f77d0355c15
shafiq-ahmed <shafiqahmd3@gmail.com>
2023-08-09 17:56:18 +0600

    Payment form finished.Source dropdown added

--- views/expense/payment.php
+++ views/expense/payment.php
@@ -8,7 +8,7 @@ use yii\widgets\ActiveForm;
 /** @var yii\web\View $this */
 /** @var app\models\Expense $model */
 
-$this->title = 'Create Expense';
+$this->title = 'Payment';
 $this->params['breadcrumbs'][] = ['label' => 'Expenses', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -20,17 +20,23 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model,'name')->textInput ( [ 'disabled' => true]) ?>
 
+    <?= $form->field($model, 'source')->dropDownList(
+            ArrayHelper::map(Sources::find()->all(),'id','name'),
+    ) ?>
 
-    <?= $form->field($model, 'amount')->textInput() ?>
+    <?= $form->field($model, 'amount')->textInput ( [ 'disabled' => true]) ?>
 
+    <?= $form->field($model, 'month')->textInput ( [ 'disabled' => true]) ?>
 
-
-
+    <?php
+    $expensedate=Yii::$app->formatter->asDate($model->expenseDate, 'dd-MM-yyyy');
+     echo $form->field($model, 'expenseDate')->textInput ( ['value' => $expensedate,'disabled'=>true])
+    ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Make Payment', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
